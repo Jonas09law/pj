de498e696a92bd233a09aa5140d083823bea8505
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-12-10 23:29:10 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,24 +9,24 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Polícia Judiciária",
-  description: "Website institucional da Polícia Judiciária Portuguesa",
-  generator: "v0.app",
+  description: "Polícia Judiciária SkyLine",
+  generator: ".vxtz.",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/pj.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/pj.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/pj.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/pj.png",
   },
 }
 
