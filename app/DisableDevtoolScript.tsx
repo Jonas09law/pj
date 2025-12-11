6ebe96dc2015c03e1f7fbc48976d26cd2b2db8a2
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-12-11 00:04:33 +0000

    Create DisableDevtoolScript.tsx

---
+++ app/DisableDevtoolScript.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Script from "next/script";
+
+export function DisableDevtoolScript() {
+  return (
+    <Script
+      src="https://cdn.jsdelivr.net/npm/disable-devtool"
+      strategy="afterInteractive"
+      onLoad={() => console.log(":))")}
+    />
+  );
+}
